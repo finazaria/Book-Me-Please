618193aa09c26b8d0249d4f4ad795fb28139d519
Bimabara <bimabara7@gmail.com>
2021-10-26 12:11:46 +0700

    base.html

--- mancay/migrations/0002_genre.py
+++ mancay/migrations/0002_genre.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sample', '0001_initial'),
+        ('mancay', '0001_initial'),
     ]
 
     operations = [
