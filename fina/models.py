5dd83995604ddfe7d6d32e9aa7e8ed33de913a47
Bimabara <bimabara7@gmail.com>
2021-12-30 21:50:58 +0700

    repush

--- fina/models.py
+++ fina/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     member_since = models.DateField(default=date.today())
+    
 
     profile_pic = models.ImageField(null=True, blank=True)
 
